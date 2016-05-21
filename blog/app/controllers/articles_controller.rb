9716ac37ce6a5963468a76e989fdc1b4270a280c
nbkhope <ryumemaru@yahoo.com>
2016-05-20 22:02:55 -0700

    Refactor articles#create and add private helper method article_params to whitelist controller parameters

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,10 +3,16 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		# We have to whitelist our controller parameters to prevent wrongful mass assignment
-		@article = Article.new(params.require(:article).permit(:title, :text))
+		@article = Article.new(article_params)
  
 	  @article.save
 	  redirect_to @article
 	end
+
+	private
+
+	# We have to whitelist our controller parameters to prevent wrongful mass assignment
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
