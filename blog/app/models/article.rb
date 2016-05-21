58ace050639148a3ce7642f2cc179f51da403122
nbkhope <ryumemaru@yahoo.com>
2016-05-21 12:04:00 -0700

    Add validation for title to create new article and display error messages in articles#new

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+	# ensure that all articles have a title that is at least five characters long
+	validates :title, presence: true,
+                    length: { minimum: 5 }
 end
