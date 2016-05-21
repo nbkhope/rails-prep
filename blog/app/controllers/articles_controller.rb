49663762d9360886482c6109dd105a0139079f26
nbkhope <ryumemaru@yahoo.com>
2016-05-20 22:01:46 -0700

    Whitelist controller parameters in articles#create

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params[:article])
+		# We have to whitelist our controller parameters to prevent wrongful mass assignment
+		@article = Article.new(params.require(:article).permit(:title, :text))
  
 	  @article.save
 	  redirect_to @article
