3c8b3e1d52365996cd1028fa8be2d445ae60c64c
nbkhope <ryumemaru@yahoo.com>
2016-05-21 19:22:28 -0700

    Add link to delete comment

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -6,6 +6,16 @@ class CommentsController < ApplicationController
 		redirect_to article_path(@article)
 	end
 
+	# The destroy action will find the article we are looking at, locate the 
+	# comment within the @article.comments collection, and then remove it from 
+	# the database and send us back to the show action for the article.
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
 	private
 	  # whitelist parameters
 	  def comment_params
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-	has_many :comments
+	# If an article is deleted, also delete all its associated comments
+	has_many :comments, dependent: :destroy
 	# ensure that all articles have a title that is at least five characters long
 	validates :title, presence: true,
                     length: { minimum: 5 }
